aec6ee8cc4383a0ca721de2c15aba9512c8d9bd9
Marcel Lipczyński <marcellipczynski@gmail.com>
2024-06-05 20:59:09 +0200

    Added cypress job to pipeline

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       });
     },
     ...nxE2EPreset(__filename, {
-      cypressDir: 'e2e',
+      cypressDir: 'src',
       webServerCommands: {
         default: 'nx run angular-shuffling-game:serve:development',
         production: 'nx run angular-shuffling-game:serve:production',

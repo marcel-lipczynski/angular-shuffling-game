7dc83065178c619e5c498dee021dcf3d3fb614b0
Marcel Lipczyński <marcellipczynski@gmail.com>
2024-06-05 14:59:39 +0200

    Finished first test

--- e2e/src/e2e/shuffling-game.cy.js
+++ e2e/src/e2e/shuffling-game.cy.js
@@ -3,9 +3,14 @@ describe('Test shuffling game', () => {
   const PLAY_AGAIN_BUTTON = 'Play again'
   const RESET_SCORE_BUTTON = 'Reset score'
 
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.intercept({url: '*/people/*', method: 'GET'}).as('getPeople');
+    cy.intercept({url: '*/starships/*', method: 'GET'}).as('getStarships');
+    cy.visit('/')
+  });
 
   it('should display game page properly without score', () => {
+
     cy.getByDataCy('app-title').contains(APP_TITLE);
 
     cy.getByDataCy('player-one-score').eq(0);
@@ -14,5 +19,9 @@ describe('Test shuffling game', () => {
     cy.getByDataCy('play-again-btn').should('contain.text', PLAY_AGAIN_BUTTON).should('be.disabled');
     cy.getByDataCy('reset-score-btn').should('contain.text', RESET_SCORE_BUTTON).should('not.be.disabled');
     cy.getByDataCy('game-mode-toggle').should('be.visible');
+
+    cy.wait(['@getPeople', '@getStarships']);
+
+    cy.getByDataCy('game-card').should('have.length', 10);
   });
 });

1e30d98f596a0cc82864e29be913218e589d98f7
Marcel Lipczyński <marcellipczynski@gmail.com>
2024-06-05 14:25:15 +0200

    Implemented cypress test to verify elements on the page

---
+++ e2e/src/e2e/shuffling-game.cy.js
@@ -0,0 +1,18 @@
+describe('Test shuffling game', () => {
+  const APP_TITLE = 'Star Wars Shuffle'
+  const PLAY_AGAIN_BUTTON = 'Play again'
+  const RESET_SCORE_BUTTON = 'Reset score'
+
+  beforeEach(() => cy.visit('/'));
+
+  it('should display game page properly without score', () => {
+    cy.getByDataCy('app-title').contains(APP_TITLE);
+
+    cy.getByDataCy('player-one-score').eq(0);
+    cy.getByDataCy('player-two-score').eq(0);
+
+    cy.getByDataCy('play-again-btn').should('contain.text', PLAY_AGAIN_BUTTON).should('be.disabled');
+    cy.getByDataCy('reset-score-btn').should('contain.text', RESET_SCORE_BUTTON).should('not.be.disabled');
+    cy.getByDataCy('game-mode-toggle').should('be.visible');
+  });
+});
